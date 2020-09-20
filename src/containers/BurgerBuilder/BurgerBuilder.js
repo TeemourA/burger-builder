56d62e6e5f1ff4d820f484f5f5d6fdcfa9c094cf
TeemourA <taxprogramming@gmail.com>
2020-09-20 20:54:09 +0300

    fix package.json

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,7 +4,6 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
-<<<<<<< HEAD
   state = {
     ingredients: {
       salad: 0,
@@ -22,29 +21,6 @@ class BurgerBuilder extends Component {
       </Aux>
     );
   }
-=======
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {...}
-    // }
-    state = {
-        ingredients: {
-            salad: 0,
-            bacon: 0,
-            cheese: 0,
-            meat: 0
-        }
-    }
-
-    render () {
-        return (
-            <Aux>
-                <Burger ingredients={this.state.ingredients} />
-                <div>Build Controls</div>
-            </Aux>
-        );
-    }
->>>>>>> 17f4b2ddba4cee8df37a56ed5af46225a4ad3042
 }
 
 export default BurgerBuilder;
