98f70047506fb0402531342fa8820a790d853b2e
TeemourA <taxprogramming@gmail.com>
2020-09-26 22:03:16 +0300

    add layout for backdrop & logic for purchase cancel

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 import classes from './Modal.module.css';
+import Aux from '../../../hoc/Aux';
+import Backdrop from '../Backdrop/Backdrop';
 
 const modal = (props) => (
-  <div
+<Aux>
+  <Backdrop show={props.show} clicked={props.modalClosed} />
+<div
     className={classes.Modal}
     style={{
       transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
@@ -11,6 +15,10 @@ const modal = (props) => (
     }}>
     {props.children}
   </div>
+
+</Aux>
+
+ 
 );
 
 export default modal;
