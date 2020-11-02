c27b374a5f0538332514c455282375b574b6d2da
Timur Akhmedov <2>
2020-11-02 18:51:58 +0300

    rename Aux dir/file & fix paths

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Aux from '../../hoc/Aux/Aux';
+import Aux from '../../hoc/Auxiliary/Auxiliary';
 import classes from './Layout.module.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
